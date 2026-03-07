386329bc578fe93cf9c923a271b1ce38f84038cc
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2026-03-07 11:01:36 +0200

    feat: add CourseSeeder to populate course data in the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             FacultySeeder::class,
             DepartmentSeeder::class,
+            CourseSeeder::class,
             ProfessorSeeder::class,
             EmployeeSeeder::class,
             StudentSeeder::class,
